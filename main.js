ecbee0f8eb845b32734257927ec050e42aeba728
Mathieu Colmon <mathieu@usp-3.fr>
2021-02-17 16:27:24 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,129 @@
+const https = require('https');
+const crypto = require('crypto');
+
+function request(method = 'GET', path = '', headers = {}) {
+  return new Promise((cb, errCb) => {
+    https.request({
+      method,
+      hostname: 'openapi.tuyaeu.com',
+      path: `/v1.0/${path}`,
+      headers,
+      maxRedirects: 20,
+    }, (res) => {
+      let data = '';
+      res.on('data', (d) => data += d);
+      res.on('error', () => {
+        errCb(new Error('Request error'));
+      });
+      res.on('close', () => {
+        try {
+          data = JSON.parse(data);
+        } catch (err) {
+          errCb(new Error('Can\'t parse server response'));
+        }
+        if (typeof data === 'object') cb(data);
+      });
+    }).end();
+  });
+}
+
+function Requester(clientID, secret) {
+  function calcSign(t) {
+    return crypto.createHmac('sha256', secret).update(`${clientID}${t}`).digest('hex').toUpperCase();
+  }
+  function calcSign2(t) {
+    return crypto.createHmac('sha256', secret).update(`${clientID}${accessToken}${t}`).digest('hex').toUpperCase();
+  }
+
+  let refreshToken = '';
+  let accessToken = '';
+  let expireToken = 0;
+
+  async function getRefreshToken() {
+    const t = Date.now();
+    const sign = calcSign(t);
+
+    const rs = request('GET', `token/${refreshToken}`, {
+      client_id: clientID,
+      t, sign,
+      sign_method: 'HMAC-SHA256',
+    });
+    if (rs.result && rs.result.access_token && rs.result.refresh_token) {
+      refreshToken = rs.result.refresh_token;
+      accessToken = rs.result.access_token;
+      expireToken = t + (rs.result.expire_time * 1000);
+      return;
+    } else {
+      throw new Error('Can\'t refresh token');
+    }
+  }
+
+  return {
+    async getAccessToken() {
+      const t = Date.now();
+      const sign = calcSign(t);
+
+      const rs = await request('GET', 'token?grant_type=1', {
+        client_id: clientID,
+        t, sign,
+        sign_method: 'HMAC-SHA256',
+      });
+
+      if (rs.result && rs.result.access_token && rs.result.refresh_token) {
+        refreshToken = rs.result.refresh_token;
+        accessToken = rs.result.access_token;
+        expireToken = t + (rs.result.expire_time * 1000);
+        return rs;
+      } else {
+        throw new Error('Can\'t get token');
+      }
+    },
+
+    async rq(deviceId, path = '', method = 'GET') {
+      if (Date.now() > expireToken) await getRefreshToken();
+
+      const t = Date.now();
+      const sign = calcSign2(t);
+
+      return request(method, `devices/${deviceId}${path}`, {
+        client_id: clientID,
+        t, sign,
+        access_token: accessToken,
+        sign_method: 'HMAC-SHA256',
+      });
+    },
+  };
+}
+
+module.exports = {
+  connect(credentials = { clientID: '', secret: '' }) {
+    return new Promise(async (cb, errCb) => {
+      if (!credentials || !credentials.clientID || !credentials.secret) {
+        errCb(new Error('Please specify Tuya Cloud "Client ID" and "Secret"'));
+        return;
+      }
+
+      const requester = new Requester(credentials.clientID, credentials.secret);
+
+      const accesToken = await requester.getAccessToken();
+
+      if (!accesToken.success) {
+        errCb(new Error('Wrong "Cloud ID" or "Secret"'));
+        return;
+      }
+
+      cb({
+        device(deviceId) {
+          return {
+            async getInfos() {
+              return (await requester.rq(deviceId)).result;
+            },
+            async getStatus() {
+              return (await requester.rq(deviceId, '/status')).result;
+            },
+          }
+        },
+      });
+    });
+  },
+};
---
+++ test.js
@@ -0,0 +1,55 @@
+const tuyaCloud = require('./main');
+const readline = require('readline');
+const fs = require('fs');
+
+if (!fs.existsSync('./credentials.json') || !fs.statSync('./credentials.json').isFile()) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  console.clear();
+  rl.question('What is your Tuya Cloud "Client ID" ? ', (clientID) => {
+    console.clear();
+    rl.question('What is your Tuya Cloud "Secret" ? ', (secret) => {
+      console.clear();
+      rl.close();
+
+      if (!clientID || !secret) process.exit(200);
+
+      init({ clientID, secret }, true);
+    });
+  });
+} else {
+  let credentials = null;
+  try {
+    credentials = JSON.parse(fs.readFileSync('./credentials.json', { encoding: 'utf8' }));
+  } catch (error) {
+    fs.unlinkSync('./credentials.json');
+    console.error('Wrong "Cloud ID" or "Secret", please retry...');
+    process.exit(200);
+  }
+
+  init(credentials);
+}
+
+function init(credentials, save = false) {
+  tuyaCloud.connect(credentials).then(async (tuya) => {
+    if (save) fs.writeFileSync('./credentials.json', JSON.stringify(credentials), { encoding: 'utf8' });
+
+    console.log('Logged !')
+
+    const tempSensor = tuya.device('xxxxxxxxxxxxxxxxxxxxxx');
+
+    setInterval(async () => {
+      console.log(await tempSensor.getInfos());
+      console.log(await tempSensor.getStatus());
+    }, 10000);
+
+  }).catch((error) => {
+    if (fs.existsSync('./credentials.json')) fs.unlinkSync('./credentials.json');
+    console.error(error.message);
+  });
+}
+
+setInterval(() => {}, 2000);
